a6cd1a3b2dd46e35d2e30a12aea3a7f81c1f5511
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-13 15:13:42 +0200

    subidas

--- application/views/peques/admin_web_view.php
+++ application/views/peques/admin_web_view.php
@@ -1,7 +1,7 @@
 <?php if ($admin) {
 	?>
 	<div id="admin">
-		<a href="/<?= RUTA_PORTAL ?>#!admin/nueva_noticia" class="nueva_noticia" >
+		<a href="/<?= RUTA_PORTAL ?>#!admin/nueva_noticia" class="nueva_noticia" >a
 			<img align="absmiddle" src="<?= PATH_IMG ?>1x1.gif" width="32" height="32"><?= $this->lang->line('admin_nueva_noticia') ?>
 		</a>
 		<a href="/<?= RUTA_PORTAL ?>#!admin/configurar_web/" class="config_Web">
