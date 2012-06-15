c3c37e9317fac4879c0192b03f172617e0ab7a05
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-15 17:37:47 +0200

    testeo correo

---
+++ application/controllers/extras/correo.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Correo extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('my_file_upload_helper');
+		$this->load->model('Usuario_model');
+		$this->load->model('Usuario_configuracion_model');
+		
+	}
+	public function prueba()
+	{
+		$correo=$this->input->get('correo');
+		if (isset($correo))
+			sendEmail($correo,'titulo correo', 'texto informativo enviado a '. $correo);
+	}
+	
+	
+	
+	
+	
+	
+}
