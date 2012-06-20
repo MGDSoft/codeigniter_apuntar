c749f3eced741368d1c6481b01b02f8fd0ec5869
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-20 06:27:46 +0200

    pequeños ajustes

--- application/views/peques/portada_contenido_view.php
+++ application/views/peques/portada_contenido_view.php
@@ -4,7 +4,7 @@
 		<ul style="margin-top:30px">
 			<li>Apunta todo lo que necesites rápidamente, muy fácil, intuitivo y gratis.</li>
 			<li>Sin necesidad de tener el navegador abierto puedes descargar nuestra aplicación para escritorio, compatible con Windows y Linux (Necesario tener instalado java 1.6).</li>
-			<li>Si necesitas acceder desde otros elementos como movil o tables es accesible de manera web y también en breves con aplicación para cada movil (Android, iPhone, blackberry)</li>
+			<li>Si necesitas acceder desde otros elementos como movil o tablets es accesible de manera web y también en breves con aplicación para movil (Android, iPhone, blackberry)</li>
 			<li>La dirección de tu propio "blog" sería &#60;TU NOMBRE&#62;.<?= URL_BASE ?>, fácil de recordar no?</li>
 			<li>Puedes hacer público o privado tu blog e incluso cada noticia pudiendo ser también comentable o no.</li>
 			<li>Opciones de decoración de tu blog para que sea único</li>
