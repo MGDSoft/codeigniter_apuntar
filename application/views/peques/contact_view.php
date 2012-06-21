67c2f021dc2c0cd48ab3b5422507d3f2b6eb4d7d
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-21 03:47:25 +0200

    arreglo email

--- application/views/peques/contact_view.php
+++ application/views/peques/contact_view.php
@@ -5,7 +5,7 @@
  			<?= (($usuario_configuracion->contacto_youtube != "")? '<a href="'.$usuario_configuracion->contacto_youtube.'" target="_blank"><img width="32" height="32"  class="youtube" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
  			
  			<?= (($usuario_configuracion->contacto_tuenti != "")? '<a href="'.$usuario_configuracion->contacto_tuenti.'" target="_blank"><img width="32" height="32" class="tuenti" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
- 			<?= (($usuario_configuracion->contacto_steam != "")? '<a href="'.$usuario_configuracion->contacto_email.'" target="_blank"><img width="32" height="32"  class="email" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
+ 			<?= (($usuario_configuracion->contacto_email != "")? '<a href="'.$usuario_configuracion->contacto_email.'" target="_blank"><img width="32" height="32"  class="email" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
  			<?= (($usuario_configuracion->contacto_twitter != "")? '<a href="'.$usuario_configuracion->contacto_twitter.'" target="_blank"><img width="32" height="32"  class="twitter" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
  			<?= (($usuario_configuracion->contacto_facebook != "")? '<a href="'.$usuario_configuracion->contacto_facebook.'" target="_blank"><img width="32" height="32"  class="facebook" src="'.PATH_IMG.'1x1.gif"></a>' : '' )?>
  			
