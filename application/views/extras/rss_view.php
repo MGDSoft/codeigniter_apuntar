4008ead35a0d6941c923d13869a922f6524d7abb
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-22 04:42:17 +0200

    rss arreglos

--- application/views/extras/rss_view.php
+++ application/views/extras/rss_view.php
@@ -1,27 +1,31 @@
-<xml version="1.0" encoding="utf-8">
-<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:admin="http://webns.net/mvcb/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">
-<channel>
-    <title><?=$titulo?></title>
-    <link><?=$feed_url?></link>
-    <atom:link href="<?=$feed_url?>/feed" rel="self" type="application/rss+xml" />
-    <description><?=$description?></description>
-    <pubDate><?=$pubdate?></pubDate>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <lastBuildDate><?=$pubdate?></lastBuildDate>
-    <docs>http://www.rssboard.org/rss-specification</docs>
-    <generator>MGDSoftware</generator>
-    <managingEditor>MGDSoftware</managingEditor>
-    <webMaster>MGDSoftware</webMaster>
-<?php foreach ($noticias as $post): ?>
-    <item>
-        <title><?=$post->titulo?></title>
-        <link><?=base_url().$nombre_unico.'#!news/' . urlencode($post->titulo).'/'.$post->id_noticia.'/' ?></link>
-        <pubDate><?= $post->fecha ?></pubDate>
-        <dc:creator>MGDSoftware</dc:creator>
-        <category><![CDATA[ <?=$post->nombre?> ]]></category>
-        <content:encoded><![CDATA[ <?=  strip_tags ($post->noticia) ?> ]]></content:encoded>
-    </item>    
-<?php endforeach;?>
-</channel>
+<?php
+echo '<?xml version="1.0" encoding="utf-8"?>' . "
+";
+?>
+<rss version="2.0" 
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:georss="http://www.georss.org/georss"
+	xmlns:media="http://search.yahoo.com/mrss/"
+>
+	<channel>
+	    <title><?=$titulo?></title>
+	    <link><?=$feed_url?></link>
+	    <atom:link href="<?=$feed_url?>/extra/rss" rel="self" type="application/rss+xml" />
+	    <description><?=$description?></description>
+	    <pubDate><?=$pubdate?></pubDate>
+	    <generator>http://www.mgdsoftware.com/</generator>
+	<?php foreach ($noticias as $post): ?>
+	    <item>
+	        <title><?=$post->titulo?></title>
+	        <dc:creator><?= $titulo ?></dc:creator>
+	        <link>http://<?=$nombre_unico.'.'.URL_BASE.'/portal#!news/' . url_title($post->titulo).'/'.$post->id_noticia.'/' ?></link>
+	        <pubDate><?= $post->fecha ?></pubDate>
+	        <category><![CDATA[<?=$post->nombre?>]]></category>
+	        <description><![CDATA[<?=  preg_replace('~[\r\n]+~', '<br>', $post->noticia) ?>]]></description>
+	    </item>    
+	<?php endforeach;?>
+	</channel>
 </rss>
