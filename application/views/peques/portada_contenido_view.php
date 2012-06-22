6a919416aacc5f504094ac43dbab8f2d47ddbbb1
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-22 12:50:14 +0200

    tu titulo

--- application/views/peques/portada_contenido_view.php
+++ application/views/peques/portada_contenido_view.php
@@ -8,7 +8,7 @@
 			<li>Apunta todo lo que necesites rápidamente, muy fácil, intuitivo y gratis.</li>
 			<li>Sin necesidad de tener el navegador abierto puedes descargar nuestra aplicación para escritorio, compatible con Windows y Linux (Necesario tener instalado java 1.6).</li>
 			<li>Si necesitas acceder desde otros elementos como movil o tablets es accesible de manera web y también en breves con aplicación para movil (Android, iPhone, blackberry)</li>
-			<li>La dirección de tu propio "blog" sería &#60;TU NOMBRE&#62;.<?= URL_BASE ?>, fácil de recordar no?</li>
+			<li>La dirección de tu propio "blog" sería &#60;Tu título&#62;.<?= URL_BASE ?>, fácil de recordar no?</li>
 			<li>Puedes hacer público o privado tu blog e incluso cada noticia pudiendo ser también comentable o no.</li>
 			<li>Opciones de decoración de tu blog para que sea único</li>
 			<li>Editor de texto con opción especial para desarrolladores para que pueda pegar su código de programación y lo puedas ver con su sintaxis coloreada</li>
