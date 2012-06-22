4b53c3bdc05912b66f0696f3858d4067286697c5
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-22 05:18:44 +0200

    rss

--- application/views/extras/rss_view.php
+++ application/views/extras/rss_view.php
@@ -1,30 +1,38 @@
 <?php
-echo '<?xml version="1.0" encoding="utf-8"?>' . "
+echo '<?xml version="1.0" encoding="UTF-8"?>' . "
 ";
 ?>
 <rss version="2.0" 
-	xmlns:atom="http://www.w3.org/2005/Atom"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
 	xmlns:dc="http://purl.org/dc/elements/1.1/"
-	xmlns:georss="http://www.georss.org/georss"
-	xmlns:media="http://search.yahoo.com/mrss/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
 >
 	<channel>
 	    <title><?=$titulo?></title>
-	    <link><?=$feed_url?></link>
-	    <atom:link href="<?=$feed_url?>/extra/rss" rel="self" type="application/rss+xml" />
+	    <link><?=$feed_url?>/extra/rss</link>
+	    <atom:link href="<?=$nombre_unico.'.'.URL_BASE ?>/extra/rss" rel="self" type="application/rss+xml" />
+	    <image>
+			<title><?=$titulo?></title>
+			<link>http://<?=$nombre_unico.'.'.URL_BASE ?></link>
+			<url>http://<?=$nombre_unico.'.'.URL_BASE.PATH_IMG.'usuarios/logo/'.$usuario_configuracion->logo ?></url>
+		</image>
 	    <description><?=$description?></description>
-	    <pubDate><?=$pubdate?></pubDate>
+	    <pubDate><?=  date(DATE_RFC822); ?></pubDate>
 	    <generator>http://www.mgdsoftware.com/</generator>
 	<?php foreach ($noticias as $post): ?>
 	    <item>
 	        <title><?=$post->titulo?></title>
 	        <dc:creator><?= $titulo ?></dc:creator>
 	        <link>http://<?=$nombre_unico.'.'.URL_BASE.'/portal#!news/' . url_title($post->titulo).'/'.$post->id_noticia.'/' ?></link>
-	        <pubDate><?= $post->fecha ?></pubDate>
+	        <guid>http://<?=$nombre_unico.'.'.URL_BASE.'/portal#!news/' . url_title($post->titulo).'/'.$post->id_noticia.'/' ?></link>
+	        <pubDate><?= date(DATE_RFC822, strtotime($post->fecha)) ?></pubDate>
 	        <category><![CDATA[<?=$post->nombre?>]]></category>
-	        <description><![CDATA[<?=  preg_replace('~[\r\n]+~', '<br>', $post->noticia) ?>]]></description>
+	        <description>
+	        	<![CDATA[<?=  preg_replace('~[\r\n]+~', '<br>', $post->noticia) ?>]]>
+	        </description>
 	    </item>    
 	<?php endforeach;?>
 	</channel>
