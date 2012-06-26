fe15c94ffe9ec3fa99abe8f19eb969a89a549191
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-26 04:31:10 +0200

    levantado la caja login

--- application/views/forms/login_Fview.php
+++ application/views/forms/login_Fview.php
@@ -3,7 +3,7 @@
 
 <form id='login_form' action="javascript:enviar_form_ajax('login_form','/forms/login_form','','','')" method="post" accept-charset="utf-8">
 <table class='formulario_estandar'>
-<tr><th colspan="2" id="login_social_devices"></th></tr>
+<?= ((isset($device) )? '<tr><th colspan="2" id="login_social_devices"></th></tr>': '') ?>
 <tr><th><?= $this->lang->line('correo') ?></th><td><input type="text" name="correo" value="" id="correo_autentificacion"></td></tr>
 <tr><th><?= $this->lang->line('contrasena') ?></th><td><input type="password" name="password" value="" id="password_autentificacion"></td></tr>
 <tr><th style="padding-top:4px"><?= (!isset($device) ? $this->lang->line('recordar') : '')  ?></th><td align="left"><input <?= (isset($device) ? 'checked="checked" style="display:none"' : '')  ?>  type="checkbox" name="recordar" id="recordar" value="ok">
