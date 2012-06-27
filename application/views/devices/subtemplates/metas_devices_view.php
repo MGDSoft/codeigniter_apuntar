2025a10df2ef0d89fb7594634a11132fa45d779b
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-27 11:26:27 +0200

    f

--- application/views/devices/subtemplates/metas_devices_view.php
+++ application/views/devices/subtemplates/metas_devices_view.php
@@ -4,18 +4,16 @@
 <?php $this->load->view('subtemplates/metas_comunes_view'); ?>
 <base href="http://<?= $_SERVER['SERVER_NAME'] ?>">
 
-<script src="/extras/cargar_archivos/js_devices" type="text/javascript"></script>
-<link href="/extras/cargar_archivos/css_devices" media="screen" rel="stylesheet"  type="text/css" />
+
 <?php if (isset($_SESSION['app']))
 {
-	 
-
 /*<script src="file:///android_asset/www/js_devices" type="text/javascript"></script>
 <link href="file:///android_asset/www/css_devices" media="screen" rel="stylesheet"  type="text/css" />
 */
 }else{
 	?>
-
+<script src="/extras/cargar_archivos/js_devices" type="text/javascript"></script>
+<link href="/extras/cargar_archivos/css_devices" media="screen" rel="stylesheet"  type="text/css" />
 <?php
 }
  $this->load->view('peques/msg_info_controller_view'); ?>
