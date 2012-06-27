ac9534f45274dfef30a1e4b0eaa1dccd9790659c
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-27 12:39:11 +0200

    ñapeando

--- application/views/devices/subtemplates/metas_devices_view.php
+++ application/views/devices/subtemplates/metas_devices_view.php
@@ -12,37 +12,13 @@
 */
 {
 ?>
+<script id="js_devices" src="/extras/cargar_archivos/js_devices" type="text/javascript"></script>
+<link id="css_devices" href="/extras/cargar_archivos/css_devices" media="screen" rel="stylesheet"  type="text/css" />
 <script type="text/javascript">
-function cargaJS(path){
-        var request = new XMLHttpRequest();
-        request.open("GET", path, true);
-        request.onreadystatechange = function(){//Call a function when the state changes.
-            console.log("state = " + request.readyState);
-            if (request.readyState == 4) {
-                if (request.status == 200 || request.status == 0) {
-                    console.log("*" + request.responseText + "*");
-                    Config = eval(request.responseText);
-                }
-            }
-        }
-        request.send();
-}
-    
-function cargaCSS(path){
-    var request = new XMLHttpRequest();
-    request.open("GET", path, true);
-    request.onreadystatechange = function(){//Call a function when the state changes.
-        console.log("state = " + request.readyState);
-        if (request.readyState == 4) {
-            if (request.status == 200 || request.status == 0) {
-            	document.write('<style>'+request.responseText+'</style>');
-            }
-        }
-    }
-    request.send();
-}
-cargaJS('file:///android_asset/www/js_devices');
-cargaCSS('file:///android_asset/www/css_devices');
+//parent.getElementById('js_devices').innerHTML;
+parent.getElementById('js_devices').innerHTML;
+document.write('<style>'+parent.getElementById('css_devices')+'</style>');
+
 </script>
 <?php 	
 }else{
