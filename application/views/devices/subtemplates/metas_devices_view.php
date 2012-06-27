5a2989082aaab30cfeaad802ae6d27f5f64cb600
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-27 10:52:52 +0200

    kitar la exclusividad de archivos locales

--- application/views/devices/subtemplates/metas_devices_view.php
+++ application/views/devices/subtemplates/metas_devices_view.php
@@ -4,17 +4,18 @@
 <?php $this->load->view('subtemplates/metas_comunes_view'); ?>
 <base href="http://<?= $_SERVER['SERVER_NAME'] ?>">
 
-
-<?php if (isset($_SESSION['app']))
-{
-?>
-<script src="file:///android_asset/www/js_devices" type="text/javascript"></script>
-<link href="file:///android_asset/www/css_devices" media="screen" rel="stylesheet"  type="text/css" />
-<?php
-}else{
-	?>
 <script src="/extras/cargar_archivos/js_devices" type="text/javascript"></script>
 <link href="/extras/cargar_archivos/css_devices" media="screen" rel="stylesheet"  type="text/css" />
+<?php if (isset($_SESSION['app']))
+{
+	 
+
+/*<script src="file:///android_asset/www/js_devices" type="text/javascript"></script>
+<link href="file:///android_asset/www/css_devices" media="screen" rel="stylesheet"  type="text/css" />
+*/
+}else{
+	?>
+
 <?php
 }
  $this->load->view('peques/msg_info_controller_view'); ?>
