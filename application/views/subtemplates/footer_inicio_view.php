ec495ee4413813370be2dc5b2e0ab34eb4a85e32
Miguel García Domínguez <miguelgd1985@gmail.com>
2012-06-29 04:21:59 +0200

    solucionado error que no se muestra la img del footer

--- application/views/subtemplates/footer_inicio_view.php
+++ application/views/subtemplates/footer_inicio_view.php
@@ -1,5 +1,5 @@
 	<div id="footer">
-		<img id="img_sobremi" <?=  ((file_exists('.'.PATH_IMG.'usuario/personal/'.$web_sobre_mi->imagen_sobre_mi) && $web_sobre_mi->imagen_sobre_mi!="") ? 'src="'. PATH_IMG.'usuario/personal/'.$web_sobre_mi->imagen_sobre_mi  : '') ?> ">
+		<img id="img_sobremi" <?=  ((file_exists('./'.PATH_IMG.'usuario/personal/'.$web_sobre_mi->imagen_sobre_mi) && $web_sobre_mi->imagen_sobre_mi!="") ? 'src="'. PATH_IMG.'usuario/personal/'.$web_sobre_mi->imagen_sobre_mi  : 'src="'. PATH_IMG.'usuario/personal/default.png') ?> ">
 		<div id="contacto_f">
 			<strong><?= $this->lang->line('contacto') ?></strong>
 			<div><?php   $this->load->view('peques/contact_view'); ?></div>
